d5377447b394bda0c1b13a23dbd543b814e45af7
E2-Lokeshwaran <lokeshwaran.s@e2infosystem.com>
2024-08-12 15:08:42 +0530

    Initial Commit

---
+++ Table_Test_Case/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Table_Test_Case
+//
+//  Created by Lokeshwaran on 12/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
