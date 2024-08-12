7bbeb80d31e41b96c2e540a8d41085079b54c54d
E2-Lokeshwaran <lokeshwaran.s@e2infosystem.com>
2024-08-12 19:35:35 +0530

    Commit 1

--- Table_Test_Case/ViewController.swift
+++ Table_Test_Case/ViewController.swift
@@ -7,13 +7,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
-    override func viewDidLoad() {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var tableData = ["1","2","3","4","5","6","7","8","9","10"]
+    override func viewDidLoad() 
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableData.count 
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell 
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TableViewCell
+        cell.cell.text = tableData[indexPath.row]
+        cell.textLabel?.text = tableData[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat 
+    {
+        return 100
+    }
 
 }
 
--- Table_Test_CaseTests/Table_Test_CaseTests.swift
+++ Table_Test_CaseTests/Table_Test_CaseTests.swift
@@ -10,27 +10,94 @@ import XCTest
 
 final class Table_Test_CaseTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    var vc : ViewController!
+    var tb : UITableView!
+    
+    override func setUpWithError() throws
+    {
+        super.setUp()
+  
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        vc = storyboard.instantiateViewController(withIdentifier: "ViewController") as? ViewController
+        vc.loadViewIfNeeded()
+        
+        tb = vc.tableView
+        
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    override func tearDownWithError() throws
+    {
+        vc = nil
+        tb = nil
+        super.tearDown()
     }
+    
+    func testViewDidLoad()
+    {
+        print("START---View loaded---")
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        vc = storyboard.instantiateViewController(withIdentifier: "ViewController") as? ViewController
+        
+        vc.loadViewIfNeeded()
+        
+        print("END---View loaded---")
+        
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func testCheckCount()
+    {
+        print("START---Test count---")
+        
+        if vc.tableData.count == 12
+        {
+            print("XCTest :- Pass")
         }
+        else
+        {
+            print("XCTest :- Fail")
+        }
+        
+        print("END---Test count---")
     }
+    
+    func testNoOfRows()
+    {
+        print("START---Test no. of rows")
+        
+        let noOfRows = vc.tableView(tb, numberOfRowsInSection: 0)
+        
+        XCTAssertEqual(noOfRows, 11,"Count mismatched")
+        
+        print("---Test no. of rows")
+    }
+    
+    func testHeight()
+    {
+        print("START---Test ht---")
+        
+        let indexPath = IndexPath(row: 0, section: 0)
+        
+        let rowHt = vc.tableView(tb,heightForRowAt: indexPath)
+        
+        XCTAssertEqual(rowHt,100,"ht should be 100")
+        
+        print("END---Test ht---")
+    }
+    
+    func testCellForRowAt() throws 
+    {
+        print("START---Test cell for---")
+        
+         let indexPath = IndexPath(row: 0, section: 0)
+        
+         let cell = vc.tableView(tb, cellForRowAt: indexPath)
+        
+        XCTAssertEqual(cell.textLabel?.text, vc.tableData[indexPath.row], "Cell text should match the corresponding item in tableData")
+        
+        print("END---Test cell for---")
+     }
+  
 
 }
